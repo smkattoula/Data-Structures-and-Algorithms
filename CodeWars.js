a3ee00715330a3a5722eb4730843431c60965459
Shaker Kattoula <smkattoula@gmail.com>
2020-09-28 15:20:38 -0700

    code wars practice

---
+++ CodeWars.js
@@ -0,0 +1,12 @@
+// Create a function that capitalizes the first letter of every word in a string
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function () {
+  return this.split(" ")
+    .map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+};
