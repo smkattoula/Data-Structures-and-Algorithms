0f67331a8ef24a6b175614131edadbe16d614bfd
Shaker Kattoula <smkattoula@gmail.com>
2020-10-16 18:12:32 -0700

    update SortingAlgorithms.js

--- SortingAlgorithms.js
+++ SortingAlgorithms.js
@@ -1,9 +1,10 @@
 // Bubble Sort
+// You're never really going to use this algorithm, it's not very efficient
+//O(n^2)
 
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
-  //O(n^2)
   const length = array.length;
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < length; j++) {
@@ -21,10 +22,11 @@ bubbleSort(numbers);
 console.log(numbers);
 
 // Selection Sort
+// Same as Bubble Sort, terrible time complexity
+// O(n^2)
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
-  // O(n^2)
   const length = array.length;
   for (let i = 0; i < length; i++) {
     // set current index as minimum
@@ -43,3 +45,145 @@ function selectionSort(array) {
 
 selectionSort(numbers);
 console.log(numbers);
+
+// Insertion Sort
+// O(n^2)
+// Use this algorithm when you have only a few items that are mostly sorted
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      //move number to the first position
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
+      if (array[i] < array[i - 1]) {
+        //find where number should go
+        for (var j = 1; j < i; j++) {
+          if (array[i] >= array[j - 1] && array[i] < array[j]) {
+            //move number to the right spot
+            array.splice(j, 0, array.splice(i, 1)[0]);
+          }
+        }
+      }
+    }
+  }
+}
+
+insertionSort(numbers);
+console.log(numbers);
+
+// Merge Sort
+// It's fast and good to use because of divide and conquer, however, it's expensive in memory.
+// O(n log n)
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+  // Split Array in into right and left
+  const length = array.length;
+  const middle = Math.floor(length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+  // console.log('left:', left);
+  // console.log('right:', right);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  // console.log(left, right)
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
+
+// Quick Sort
+// Considered even better than merge sort because it has the same time complexity but less space complexity. However, it has a huge doesnt side if you don't pick the pivot properly. Choosing the wrong pivot can increase time complexity to O(n^2).
+// O(n log n)
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array, left, right) {
+  const len = array.length;
+  let pivot;
+  let partitionIndex;
+
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+
+    //sort left and right
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+
+function partition(array, pivot, left, right) {
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
+
+// Sorting Interview Practice
+
+//#1 - Sort 10 schools around your house by distance:
+insertion sort
+
+//#2 - eBay sorts listings by the current Bid amount:
+radix or counting sort
+
+//#3 - Sort scores on ESPN
+Quick sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+Merge Sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+Insertion Sort
+
+//#6 - Temperature Records for the past 50 years in Canada
+radix or counting Sort
+Quick sort if decimal places
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+Quick sort
+
+//#8 - You want to teach sorting
+Bubble sort
